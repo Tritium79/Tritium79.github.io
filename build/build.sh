e2596ff965aff7f492e7debaf3d8e1d5ead552e3
Tritium79 <Prairial76@outlook.com>
2026-08-12 21:04:30 +0800

    2026-08-12 Update

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-cd "$(dirname "$0")"
+SCRIPT_PATH="$(realpath -- "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(cd -- "$(dirname -- "$SCRIPT_PATH")" && pwd)"
+cd "$SCRIPT_DIR" || exit 1
 source venv/bin/activate
 python3 build.py "$@"
